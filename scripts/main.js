54eb606657a9b2e035590c37d84e6fcb4ed808d2
NicoJanssonPersonal <janssnic@arcada.fi>
2025-09-25 16:16:16 +0300

    notes are drawn

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const loginForm = document.getElementById("loginForm")
 
-loginForm.addEventListener("submit", async(event) => {
+loginForm.addEventListener("submit", async (event) => {
     event.preventDefault()
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
@@ -16,13 +16,21 @@ loginForm.addEventListener("submit", async(event) => {
                 password
             })
         });
-        const data = await response.json();
-        localStorage.setItem("token", data.jwt);
-        console.debug(localStorage.getItem("token"))
-        return data;
+
+        if (!response.ok) {
+            alert("Login failed")
+            return
+        }
+        const data = await response.json()
+
+        if (data.jwt) {
+            localStorage.setItem("token", data.jwt)
+            console.debug(localStorage.getItem("token"))
+        }
+
+        window.location.href = "../boards"
     } catch (error) {
-        console.error("Login failed:", error);
-        return null;
+        console.error("Login failed:", error)
     }
 })
 
